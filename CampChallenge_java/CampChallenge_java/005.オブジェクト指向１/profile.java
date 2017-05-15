4bc7322463874744ccca9c59704b454cfceec0b5
saitonaoki <pdcaword@gmail.com>
2017-05-15 13:13:39 +0900

    Revert "no message"
    This reverts commit 43688a52dce6acfcbc4183c57cabe857becbd6cf.

--- CampChallenge_java/CampChallenge_java/005.オブジェクト指向１/profile.java
+++ CampChallenge_java/CampChallenge_java/005.オブジェクト指向１/profile.java
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package my;
+package myprofile;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,19 +17,72 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author metar
  */
-public class profile {
-    
-void myprofile(PrintWriter pw){
-pw.print("私の名前は、齋藤尚輝です<br>");
-pw.print("趣味はゲームと小説を読むことです<br>");
-pw.print("得意な事は走ることです");
-}
-protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    PrintWriter out = response.getWriter();
+@WebServlet(name = "profile", urlPatterns = {"/profile"})
+public class profile extends HttpServlet {
 
-    for(int i =1; i<=10;i++){
-    myprofile(out);
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet profile</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet profile at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
-    
-   }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
